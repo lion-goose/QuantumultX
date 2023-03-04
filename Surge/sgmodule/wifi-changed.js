c3a91b41dec5f87a3d9b1451ca3c4e942771d444
Angeliss <59443350+lion-goose@users.noreply.github.com>
2023-03-04 16:04:26 +0800

    Update wifi-changed.js

--- Surge/sgmodule/wifi-changed.js
+++ Surge/sgmodule/wifi-changed.js
@@ -1,10 +1,9 @@
 const WIFI_DONT_NEED_PROXYS = ['32612-5G'];
-const CURRENT_WIFI_SSID_KEY = 'current_wifi_ssid';
 
 if (wifiChanged()) {
   const mode = WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)
-    ? 'direct'
-    : 'rule';
+    ? 'rule'
+    : 'direct';
   $surge.setOutboundMode(mode);
   $notification.post(
     'Surge',
